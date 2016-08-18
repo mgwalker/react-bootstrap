21d50a2831b267169882be7d323d94bc9a244e36
Greg Walker <mgwalker@gmail.com>
2016-08-18 18:33:11 -0500

    Next

--- src/index.js
+++ src/index.js
@@ -28,8 +28,6 @@ const devModules = [
   "watchify"
 ];
 
-process.exit(0);
-
 exec(`npm install --save-dev ${devModules.join(' ')}`)
   .then(() => {
     return exec('mkdir -p web');
@@ -56,5 +54,29 @@ exec(`npm install --save-dev ${devModules.join(' ')}`)
     return fs.copySync(`${__dirname}/eslintrc.json`, '.eslintrc');
   })
   .then(() => {
+    return fs.copySync(`${__dirname}/bin-index.html`, 'web/bin/index.html');
+  })
+  .then(() => {
+    return fs.copySync(`${__dirname}/src-index.js`, 'web/src/index.js');
+  })
+  .then(() => {
+    return fs.copySync(`${__dirname}/src-store.js`, 'web/src/store.js');
+  })
+  .then(() => {
+    return fs.copySync(`${__dirname}/src-reducers-index.js`, 'web/src/reducers/index.js');
+  })
+  .then(() => {
+    return exec('touch web/src/style/main.scss');
+  })
+  .then(() => {
+    const pkg = require(`${process.cwd()}/package`);
+    pkg.scripts['lint'] = "eslint --fix 'web/src/**/*.js' && eslint --fix 'server/**/*.js'";
+    pkg.scripts['build-style'] = "node-sass web/src/style/main.scss -o web/bin";
+    pkg.scripts['build-html'] = "browserify -d -t [ babelify --presets [ react es2015 ] ] web/src/index.js -o web/bin/bundle.js";
+    pkg.scripts['build'] = "npm run build-style && npm run build-html";
+    pkg.scripts['watch-style'] = "node-sass -w web/src/style/ -o web/bin --include-path `node sass-paths.js`";
+    pkg.scripts['watch-js'] = "watchify -v -d -t [ babelify --presets [ react es2015 ] ] web/src/index.js -o web/bin/bundle.js";
+    pkg.scripts['watch'] = "npm run watch-js & npm run watch-style && fg";
+
     return fs.writeFileSync('package.json', JSON.stringify(pkg, false, '  '));
   })
